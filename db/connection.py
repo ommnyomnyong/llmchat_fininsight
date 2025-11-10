81ec20177173aed9c485c4e4a9dede0043f0940b
ommnyomnyong <kimiy990728@gmail.com>
2025-11-10 15:26:04 +0900

    DB 접속 방식 변경

--- db/connection.py
+++ db/connection.py
@@ -1,14 +1,24 @@
-
+import os
 ## DB 연결만 담당
 from sqlalchemy import create_engine
 
-DB_USER = "root"
-DB_PASSWORD = "root"
-DB_HOST = "127.0.0.1"
-DB_PORT = "3306"
+# DB 접속 정보
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT') or '3306'  # 환경변수 미설정 시 기본값 사용
 
+USER_DB_NAME = os.getenv('USER_DB_NAME', 'user_db')
+PROJECT_DB_NAME = os.getenv('PROJECT_DB_NAME', 'project_db')
+CHAT_DB_NAME = os.getenv('CHAT_DB_NAME', 'chat_db')
 
 # DB 연결
-user_engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/user_db", echo=True)
-project_engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/project_db", echo=True)
-chat_engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/chat_db", echo=True)
+user_engine = create_engine(
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{USER_DB_NAME}", echo=True
+)
+project_engine = create_engine(
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{PROJECT_DB_NAME}", echo=True
+)
+chat_engine = create_engine(
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{CHAT_DB_NAME}", echo=True
+)
